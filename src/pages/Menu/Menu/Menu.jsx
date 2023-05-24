3a2e63ecf20a51e10e6eb43637ca9a50250c520e
DashTfied <htanzir13@gmail.com>
2023-05-24 11:12:06 +0600

    bistro boss menu and order with server

---
+++ src/pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,46 @@
+import { Helmet } from "react-helmet-async";
+import Cover from "../../Shared/Cover/Cover";
+import menuImg from "../../../assets/menu/banner3.jpg";
+import dessertImg from "../../../assets/menu/dessert-bg.jpeg";
+import soupImg from "../../../assets/menu/soup-bg.jpg";
+import saladImg from "../../../assets/menu/salad-bg.jpg";
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/Sectiontitle/SectionTitle";
+import MenuCategory from "../MenuCategory/MenuCategory";
+
+const Menu = () => {
+  const [menu] = useMenu();
+  const desserts = menu.filter((item) => item.category === "dessert");
+  const soup = menu.filter((item) => item.category === "soup");
+  const salad = menu.filter((item) => item.category === "salad");
+  const pizza = menu.filter((item) => item.category === "pizza");
+  const offered = menu.filter((item) => item.category === "offered");
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Menu</title>
+      </Helmet>
+      <Cover img={menuImg} title="Our Menu"></Cover>
+      {/* Main cover */}
+      <SectionTitle
+        subHeading="Don't Miss"
+        heading="Today's Offer"
+      ></SectionTitle>
+      {/* Offered Items */}
+      <MenuCategory items={offered}></MenuCategory>
+      {/* Dessert Menu items */}
+      <MenuCategory
+        items={desserts}
+        title="Dessert"
+        img={dessertImg}
+      ></MenuCategory>
+      {/* Pizza Menu Items */}
+      <MenuCategory items={pizza} title={"pizza"} img={pizzaImg}></MenuCategory>
+      <MenuCategory items={salad} title={"salad"} img={saladImg}></MenuCategory>
+      <MenuCategory items={soup} title={"soup"} img={soupImg}></MenuCategory>
+    </div>
+  );
+};
+
+export default Menu;
